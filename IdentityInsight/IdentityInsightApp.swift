29fae8529b238d498959c921fc2044d9bf31e190
paulober <44974737+paulober@users.noreply.github.com>
2023-07-20 16:42:47 +0200

    Initial Commit

---
+++ IdentityInsight/IdentityInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdentityInsightApp.swift
+//  IdentityInsight
+//
+//  Created by Paul on 20.07.23.
+//
+
+import SwiftUI
+
+@main
+struct IdentityInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
